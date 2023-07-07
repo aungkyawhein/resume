f6bb94887ba767df0fd80682838b086d93a6fe09
aungkyawhein <akh.infotech@gmail.com>
2023-07-07 22:52:51 +0800

    firebaes env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { BrowserModule } from '@angular/platform-browser';
+import { firebaseConfig } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +11,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/environments/environment.template.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "AUTH_DOMAIN",
+  projectId: "PROJECT_ID",
+  appId: "APP_ID",
+  measurementId: "MEASUREMENT_ID"
+};
