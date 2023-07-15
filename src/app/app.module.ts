b7b3a94cd426d0a616f45f461094cb0565b17f5b
aungkyawhein <akh.infotech@gmail.com>
2023-07-16 00:16:22 +0800

    Added edit form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { BrowserModule } from '@angular/platform-browser';
 import { firebaseConfig } from 'src/environments/environment';
 
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EditComponent } from './edit/edit.component';
@@ -16,6 +18,8 @@ import { ViewComponent } from './view/view.component';
   ],
   imports: [
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    CommonModule,
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
