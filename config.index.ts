ca58c475d5db3e3394815690306b7c5231e35508
aungkyawhein <akh.infotech@gmail.com>
2023-07-16 14:01:09 +0800

    Add config file to support env variable for Firebase config

---
+++ config.index.ts
@@ -0,0 +1,11 @@
+import { writeFile } from 'fs';
+
+const targetPath = './src/environments/environment.prod.ts';
+
+const envConfigFile = `export const firebaseConfig = JSON.parse('${process.env.FIREBASE_CONFIG}')`;
+
+writeFile(targetPath, envConfigFile, 'utf8', (err) => {
+  if (err) {
+    return console.log(err);
+  }
+});
